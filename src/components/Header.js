66e4a906e5d5b3b63edeb21a586e1309eb535d1f
Mrigakshi Roy Choudhury <mrigakshirc@gmail.com>
2023-03-13 18:29:49 +0530

    added the header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import styles from "./Header.module.css";
+import React from "react";
+function Header() {
+  return (
+    <React.Fragment>
+      <header className={styles.header}>
+      </header>
+    </React.Fragment>
+  );
+}
+
+export default Header;
