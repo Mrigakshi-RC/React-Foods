cb67adb70253757edf0ee468de6f98f6c8122fb0
Mrigakshi Roy Choudhury <mrigakshirc@gmail.com>
2023-03-14 17:19:58 +0530

    completed the header part

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-import Header from "./components/Header"
+import Header from "./components/Header";
+import MealsSummary from "./components/MealsSummary";
 
 function App() {
   return (
     <React.Fragment>
       <Header/>
+      <MealsSummary/>
     </React.Fragment>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,23 @@
 import styles from "./Header.module.css";
+import classes from "./HeaderCartButton.module.css";
+import CartIcon from "./CartIcon"
+import background from "../images/meals.jpg"
 import React from "react";
+
 function Header() {
   return (
     <React.Fragment>
       <header className={styles.header}>
+        <h1>ReactMeals</h1>
+        <div className={classes.button}>
+          <CartIcon className={classes.icon}/>
+          <p>Your Cart</p>
+          <div className={classes.badge}>0</div>
+        </div>
       </header>
+      <div className={styles.mainImage}>
+        <img src={background} alt="background"></img>
+      </div>
     </React.Fragment>
   );
 }
