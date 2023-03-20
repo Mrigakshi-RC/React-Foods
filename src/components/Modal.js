adb684e4c139c9a62a001cc03ada48a4ed83dd99
Mrigakshi Roy Choudhury <mrigakshirc@gmail.com>
2023-03-20 16:55:50 +0530

    improved sematics by creating new portal for modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,21 +1,39 @@
 import React from "react";
 import styles from "./Modal.module.css";
+import ReactDOM from "react-dom";
+
+const Backdrop = () => {
+  return <div className={styles.backdrop}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={styles.modal}>
+      <div className={styles.totalContainer}>
+        <h3>Total Amount</h3>
+        <h3>$0.00</h3>
+      </div>
+      <div className={styles.buttonContainer}>
+        <button onClick={props.close} className={styles.close}>
+          close
+        </button>
+        <button className={styles.order}>Order</button>
+      </div>
+    </div>
+  );
+};
 
 const Modal = (props) => {
   return (
     <React.Fragment>
-      <div className={styles.backdrop}>
-        <div className={styles.modal}>
-          <div className={styles.totalContainer}>
-            <h3>Total Amount</h3>
-            <h3>$0.00</h3>
-          </div>
-          <div className={styles.buttonContainer}>
-            <button onClick={props.close} className={styles.close}>close</button>
-            <button className={styles.order}>Order</button>
-          </div>
-        </div>
-      </div>
+      {ReactDOM.createPortal(
+        <Backdrop />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay close={props.close} />,
+        document.getElementById("overlay-root")
+      )}
     </React.Fragment>
   );
 };
