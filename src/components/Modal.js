3c2690a79b01fe82c8460b032d25636f7e03ee55
Mrigakshi Roy Choudhury <mrigakshirc@gmail.com>
2023-03-23 16:53:53 +0530

    got started with the order-cart

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, {useContext} from "react";
 import styles from "./Modal.module.css";
+import classes from "./Cart/Cart.module.css";
 import ReactDOM from "react-dom";
+import CartContext from "../store/cart-context";
+import CartItem from "./Cart/CartItem";
 
 const Backdrop = (props) => {
   return <div className={styles.backdrop} onClick={props.close}></div>;
 };
 
 const ModalOverlay = (props) => {
+  const cartCtx= useContext(CartContext);
+
+  const totalAmount=`$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems= cartCtx.items.length>0;
+
+  const cartItemRemoveHandler = id =>{};
+  const cartItemAddHandler = item =>{};
+
   const cartItems = (
-    <ul className={styles["cart-items"]}>
-      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
-        <li>{item.name}</li>
+    <ul className={classes["cart-items"]}>
+      {cartCtx.items.map((item) => (
+        <CartItem key={item.id} name={item.name} amount={item.amount} price= {item.price} onRemove={cartItemRemoveHandler.bind(null,item.id)} onAdd={cartItemAddHandler.bind(null,item)}/>
       ))}
     </ul>
   );
@@ -20,13 +31,13 @@ const ModalOverlay = (props) => {
       {cartItems}
       <div className={styles.totalContainer}>
         <h3>Total Amount</h3>
-        <h3>$0.00</h3>
+        <h3>{totalAmount}</h3>
       </div>
       <div className={styles.buttonContainer}>
         <button onClick={props.close} className={styles.close}>
           close
         </button>
-        <button className={styles.order}>Order</button>
+        {hasItems && <button className={styles.order}>Order</button>}
       </div>
     </div>
   );
