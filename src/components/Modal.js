d2ab7157473b3bf0f79a02f8131274de32faa28b
Mrigakshi Roy Choudhury <mrigakshirc@gmail.com>
2023-03-23 17:34:45 +0530

    implemented add and remove features in the cart

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import styles from "./Modal.module.css";
 import classes from "./Cart/Cart.module.css";
 import ReactDOM from "react-dom";
@@ -10,18 +10,29 @@ const Backdrop = (props) => {
 };
 
 const ModalOverlay = (props) => {
-  const cartCtx= useContext(CartContext);
+  const cartCtx = useContext(CartContext);
 
-  const totalAmount=`$${cartCtx.totalAmount.toFixed(2)}`;
-  const hasItems= cartCtx.items.length>0;
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = id =>{};
-  const cartItemAddHandler = item =>{};
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({...item, amount:1});
+  };
 
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
-        <CartItem key={item.id} name={item.name} amount={item.amount} price= {item.price} onRemove={cartItemRemoveHandler.bind(null,item.id)} onAdd={cartItemAddHandler.bind(null,item)}/>
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
