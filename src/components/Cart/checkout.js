d319d311d82c4537009a3f4a40c91517a0affa0a
Mrigakshi Roy Choudhury <mrigakshirc@gmail.com>
2023-04-22 06:33:05 +0530

    added styling to the checkout form

--- src/components/Cart/checkout.js
+++ src/components/Cart/checkout.js
@@ -1,29 +1,33 @@
-import classes from "./checkout.module.css";
+import classes from './checkout.module.css';
+
 const Checkout = (props) => {
   const confirmHandler = (event) => {
     event.preventDefault();
   };
+
   return (
-    <form>
+    <form className={classes.form} onSubmit={confirmHandler}>
       <div className={classes.control}>
-        <label htmlFor="name">Your Name</label>
-        <input type="text" id="name"></input>
+        <label htmlFor='name'>Your Name</label>
+        <input type='text' id='name' />
       </div>
       <div className={classes.control}>
-        <label htmlFor="street">Street</label>
-        <input type="text" id="street"></input>
+        <label htmlFor='street'>Street</label>
+        <input type='text' id='street' />
       </div>
       <div className={classes.control}>
-        <label htmlFor="postal">Postal Code</label>
-        <input type="number" id="postal"></input>
+        <label htmlFor='postal'>Postal Code</label>
+        <input type='text' id='postal' />
       </div>
       <div className={classes.control}>
-        <label htmlFor="city">City</label>
-        <input type="text" id="city"></input>
+        <label htmlFor='city'>City</label>
+        <input type='text' id='city' />
       </div>
-      <div className={classes.checkoutActions}>
-        <button onClick={props.onCancel}>Cancel</button>
-        <button onClick={confirmHandler}>Confirm</button>
+      <div className={classes.actions}>
+        <button type='button' onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={classes.submit}>Confirm</button>
       </div>
     </form>
   );
